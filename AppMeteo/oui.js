fa8b9e9e90be474322bd42bd5ba86295e0686fee
tacosougrecDEV <103994693+tacosougrecDEV@users.noreply.github.com>
2023-10-12 22:45:48 +0200

    Add files via upload
    Commit project

---
+++ AppMeteo/oui.js
@@ -0,0 +1,10 @@
+<div id = "today" >
+	<h1>
+	 Marseille(FR) 
+	</h1>
+	<img src="https:/ / openweathermap.org / img / wn / 01 d @2x.png ">
+	<h1>22°C</h1>
+	<h3>Ressenti: 22°C</h3>
+	<h3>Humidité: 61%</h3>
+	<h3>Vent: 4.47km/h</h3>
+</div>
