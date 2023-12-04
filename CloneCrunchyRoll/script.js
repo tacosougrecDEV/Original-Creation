af950f9ebed6de05f2ad4313e4b009eaa2628341
tacosougrecDEV <sansberrot@gmail.com>
2023-12-04 17:36:37 +0100

    CloneCrunchyroll
    I'm trying to make a clone of crunchyroll to apply to it what I'm learning in BeginJavascript

---
+++ CloneCrunchyRoll/script.js
@@ -0,0 +1,4 @@
+const naviguer = document.getElementById("naviguer");
+const news = document.getElementById("news");
+const naviguerMore = document.getElementById("naviguerMore");
+const newsMore = document.getElementById("newsMore");
