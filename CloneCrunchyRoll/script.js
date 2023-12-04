f2ea2f20bb7d86ab624d39c8ef673380c1fe5678
tacosougrecDEV <sansberrot@gmail.com>
2023-12-04 18:57:36 +0100

    "naviguer" tab
    I made the "naviguer" tab. I have to make "news" tab

--- CloneCrunchyRoll/script.js
+++ CloneCrunchyRoll/script.js
@@ -1,4 +1,13 @@
-const naviguer = document.getElementById("naviguer");
+const naviguer = document.getElementById("liNaviguer");
 const news = document.getElementById("news");
 const naviguerMore = document.getElementById("naviguerMore");
 const newsMore = document.getElementById("newsMore");
+
+naviguer.addEventListener("click", () => {
+    naviguer.classList.toggle("active");
+    naviguerMore.classList.toggle("naviguerVisible");
+});
+news.addEventListener("click", () => {
+    news.classList.toggle("active");
+    newsMore.classList.toggle("newsVisible");
+});
